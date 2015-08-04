ca4bd4d52eea399fd6fb90e9976e618e5a8fb9bb
ashwin153 <ashwin.madavan@gmail.com>
2015-08-04 16:05:35 -0700

    Added performance tests using the Google Caliper benchmarking framework

---
+++ src/perf/java/com/chunkr/compress/ChunkerBenchmark.java
@@ -0,0 +1,66 @@
+package com.chunkr.compress;
+
+import com.chunkr.compress.chunkers.ModifiedChunker;
+import com.chunkr.compress.chunkers.StandardChunker;
+import com.google.caliper.BeforeExperiment;
+import com.google.caliper.Benchmark;
+import com.google.caliper.Param;
+import com.google.caliper.api.VmOptions;
+import com.google.caliper.runner.CaliperMain;
+
+@VmOptions("-server")
+public class ChunkerBenchmark {
+	
+	public static void main(String[] args) {
+		CaliperMain.main(ChunkerBenchmark.class, args);
+	}
+	
+	@Param({ "5", "6", "7" })
+	private int _chunkSize;
+	
+	@Param({ "STANDARD", "MODIFIED" })
+	private Method _method;
+	
+	private Chunker _chunker;
+	private boolean[] _bits;
+	private int[] _chunks;
+	
+	public enum Method {
+		
+		STANDARD {
+			@Override
+			public Chunker getChunker(int chunkSize) {
+				return new StandardChunker(chunkSize);
+			}
+		},
+		
+		MODIFIED {
+			@Override public Chunker getChunker(int chunkSize) {
+				return new ModifiedChunker(chunkSize);
+			}
+		};
+		
+		abstract public Chunker getChunker(int chunkSize);
+	}
+	
+	@BeforeExperiment
+	public void setUp() throws Exception {
+		_chunker = _method.getChunker(_chunkSize);
+		_bits = new boolean[_chunkSize * 10];
+		for(int i = 0; i < _bits.length; i++)
+			_bits[i] = (Math.random() < 0.50) ? true : false;
+		_chunks = _chunker.chunk(_bits);
+	}
+	
+	@Benchmark
+	public void timeUnchunk(int reps) {
+		for(int i = 0; i < reps; i++)
+			_chunker.unchunk(_chunks);
+	}
+	
+	@Benchmark
+	public void timeChunk(int reps) {
+		for(int i = 0; i < reps; i++)
+			_chunker.chunk(_bits);
+	}
+}
--- src/test/java/com/chunkr/compress/ChunkerTest.java
+++ src/test/java/com/chunkr/compress/ChunkerTest.java
@@ -10,6 +10,7 @@ import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import com.chunkr.compress.Chunker;
+import com.chunkr.compress.chunkers.ModifiedChunker;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ChunkerTest {
@@ -22,6 +23,49 @@ public class ChunkerTest {
 		boolean[] unchunks = new boolean[] { true, false, true, false };
 		int chunk = 10;
 		assertArrayEquals(unchunks, chunker.unchunk(chunk));
+	}
+	
+
+	@Test
+	public void testModifiedChunker() {		
+		for(int chunkSize = 1; chunkSize <= 12; chunkSize ++) {
+			// Generate a randomized input sequence of 1000 chunks
+			boolean[] input = new boolean[10000 * chunkSize];
+			for(int i = 0; i < input.length; i++)
+				input[i] = (Math.random() < 0.50) ? true : false;
+			
+			Chunker chunker = new ModifiedChunker(chunkSize);
+			int[] chunked = chunker.chunk(input);
+			for(double marginOfError = 0.0; marginOfError <= 0.10; marginOfError += 0.025) {
+				// Randomly alter chunked elements within the marginOfError;
+				// this is to simulate the curve fitting algorithm's imprecise
+				// calculations.
+				int[] altered = new int[chunked.length];
+				for(int i = 0; i < altered.length; i++) {
+					int sgn = (Math.random() < 0.5) ? 1 : -1;
+					int val = (int) (Math.random() * Math.ceil(Math.pow(2, chunkSize) * marginOfError + 1));
+					altered[i] = chunked[i] + sgn * val;
+
+					if(altered[i] < 0)
+						altered[i] = 0;
+					else if(altered[i] >= Math.pow(2, chunkSize))
+						altered[i] = (int) Math.pow(2, chunkSize) - 1;
+				}
+				
+				// Unchunk the altered data points and calculate the total
+				// number of bit flips between the altered unchunked data and
+				// the original input data.
+				boolean[] unchunked = chunker.unchunk(altered);
+				int bitFlips = 0;
+				for(int i = 0; i < unchunked.length; i++)
+					if(unchunked[i] != input[i])
+						bitFlips++;
+				
+				System.out.println(String.format("%3d\t%5.3f\t%20d\t%15.3f",
+						chunkSize, marginOfError, bitFlips, (double) bitFlips
+								/ input.length));
+			}
+		}
 	}
 	
 }
