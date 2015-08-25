e2e7fe7d37d3fa170367f3eeb5922765e32db4c9
ashwin153 <ashwin.madavan@gmail.com>
2015-08-25 15:27:24 -0500

    Added fast fourier regression

--- src/test/java/com/chunkr/compress/regressors/FastFourierRegressorTest.java
+++ src/test/java/com/chunkr/compress/regressors/FastFourierRegressorTest.java
@@ -1,5 +1,15 @@
 package com.chunkr.compress.regressors;
 
+import java.math.BigDecimal;
+
+import org.junit.Test;
+
 public class FastFourierRegressorTest {
 
+	@Test
+	public void testFit() {
+		int[] chunks = new int[] { 408, 89, -66, 10, 338, 807, 1238, 1511, 1583, 1462, 1183, 804};
+		FastFourierRegressor regressor = new FastFourierRegressor();
+		System.out.println(regressor.fit(chunks).eval(BigDecimal.valueOf(3)));
+	}
 }
